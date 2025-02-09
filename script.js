78b3e543d4c97cded398e62fc7ffd577bd095a57
ahmedforeducation <ahmedmomtaz445@gmail.com>
2025-02-09 20:14:36 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function toggleDropdown() {
+    var dropdown = document.getElementById("dropdown");
+    if (dropdown.style.display === "block") {
+        dropdown.style.display = "none";
+    } else {
+        dropdown.style.display = "block";
+    }
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.more-button')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === "block") {
+                openDropdown.style.display = "none";
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
